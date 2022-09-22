dabac93ed3ecbad2abc33952c5a34394e2832b7d
przemex94 <przemyslaw.aniol@ing.com>
2022-09-22 06:15:42 -0400

    instalator

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+dpkg -i chrome.deb
+cp chromedriver /bin/chromedriver
--- jellyfish.py
+++ jellyfish.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3 
 from selenium import webdriver
 print("-------------------------------------")
 print(" jellyfish.py - Clickjacking checker.")
